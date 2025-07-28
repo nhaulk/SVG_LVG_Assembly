68e373fecdf4377d570e7976d74e88a31045963e
nhaulk <nhaulk1@gmail.com>
2025-07-28 17:34:47 -0400

    combined scaffold and mitochondrial fasta files

--- scripts/genome_final.sh
+++ scripts/genome_final.sh
@@ -8,45 +8,48 @@
 #SBATCH -n 48                 #number of cores/tasks in this job, you get all 2$
 #SBATCH -t 1:00:00            #time to run
 #SBATCH --mail-user=nhaulk@utk.edu  #enter your email address to receive emails
-#SBATCH --mail-type=BEGIN,END,FAIL #will receive an email when job starts, ends$
+#SBATCH --mail-type=END,FAIL #will receive an email when job starts, ends$
 #SBATCH -o "output/Genome_clean.%j.out"     # standard output, %j adds job number to output fil$
 #SBATCH -e "output/Genome_clean.%j.err"     #optional, prints our standard error
 
 
 
-python /lustre/isaac24/proj/UTK0312/nathaniel.haulk/Dlongi_genomes/juicebox_assembly_converter.py -a /lustre/isaac24/proj/UTK0312/nathaniel.haulk/Dlongi_genomes/LVGandSVG_Juicebox/LVG_juicebox_p/LVG_Male.p_ctg_noec.review_kac_07-11-25.assembly.assembly /
--f /lustre/isaac24/scratch/nhaulk/Dlongi_genomes/LVG_Punaluu_Male/HiC_yahs_p/LVG_Male.p_ctg_noec_converted.fasta /
--p test -s
-
 
 # Define paths
-ASSEMBLY="/lustre/isaac24/proj/UTK0312/nathaniel.haulk/Dlongi_genomes/LVGandSVG_Juicebox/LVG_juicebox_p/LVG_Male.p_ctg_noec.review_kac_07-11-25.assembly.assembly"
-FASTA="/lustre/isaac24/scratch/nhaulk/Dlongi_genomes/LVG_Punaluu_Male/HiC_yahs_p/LVG_Male.p_ctg_noec_converted.fasta"
+ASSEMBLY="/lustre/isaac24/proj/UTK0312/nathaniel.haulk/projects/Dlongi_genomes/LVGandSVG_Juicebox/LVG_juicebox_p/LVG_Male.p_ctg_noec.review_kac_07-11-25.assembly.assembly"
 PREFIX="test"
+RAWDATA="/lustre/isaac24/scratch/nhaulk/Dlongi_genomes_USDA/LVG_Punaluu_Male"
 
 # Define output directory
-OUTPUTDIR="../converted_scaffold"
+OUTPUTDIR="../Clean_Genome"
 
 # Check if output directory exists; if not, create it
 if [[ -d "$OUTPUTDIR" ]]; then
   echo "$OUTPUTDIR already exists."
+
 else
   echo "Creating directory $OUTPUTDIR"
   mkdir "$OUTPUTDIR"
 fi
 
-# Run the converter
-python /lustre/isaac24/proj/UTK0312/nathaniel.haulk/Dlongi_genomes/juicebox_assembly_converter.py \
+# Move to the working directory
+cd "$OUTPUTDIR"
+
+# Convert the file from a juicebox assembly to a fasta file
+python /lustre/isaac24/proj/UTK0312/nathaniel.haulk/projects/juicebox_assembly_converter.py \
   -a "$ASSEMBLY" \
-  -f "$FASTA" \
+  -f "$RAWDATA/HiC_yahs_p/LVG_Male.p_ctg_noec_converted.fasta" \
   -p "$PREFIX" \
   -s
 
 # Run stats on the produced FASTA file
 stats.sh -Xmx4g ${PREFIX}.fasta >${PREFIX}.stats
 
+# combine the mitochondiral dNA with the scaffold info
+cat ${RAWDATA}/HiC_yahs_p/yahs/LVG_Male.p_ctg_yahsout_noec_scaffolds_final.fa ${RAWDATA}/MT_p_noec/CM067966.1.fasta > Dlongi_UGA_Male_chr_unpl_mt.fasta
 
+#!/bin/bash
 
+sed '/^>CM067966\.1 Diachasmimorpha longicaudata isolate KC_UGA_2023 mitochondrion, complete sequence, whole genome shotgun sequence/ i\
+' "Dlongi_UGA_Male_chr_unpl_mt.fasta" > "Dlongi_UGA_Male_chr_unpl_mt2.fasta"
 
-# Move output files that start with the prefix into the output directory
-mv ${PREFIX}* "$OUTPUTDIR/"
