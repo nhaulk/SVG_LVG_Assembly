f355b97fc2458a6460dea12f82077460a0cd26fb
nhaulk <nhaulk1@gmail.com>
2025-10-16 10:36:32 -0400

    Reran dot files

--- scripts/synteny.sh
+++ scripts/synteny.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=campus
 #SBATCH -N 1                #number of nodes in this job
 #SBATCH -n 48                 #number of cores/tasks in this job, you get all 2$
-#SBATCH -t 24:00:00            #time to run
+#SBATCH -t 3:00:00            #time to run
 #SBATCH --mail-user=nhaulk@utk.edu  #enter your email address to receive emails
 #SBATCH --mail-type=END,FAIL #will receive an email when job starts, ends$
 #SBATCH -o "output/FtoD.%j.out"     # standard output, %j adds job number to output fil$
